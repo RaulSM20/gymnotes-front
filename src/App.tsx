8c20cdb9a15c96fd41f7527c94177f83f129efaf
rsandez <raulsandezmartinez@gmail.com>
2024-08-28 13:25:37 +0200

    update login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Button } from './components/ui/button'
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { Button } from "./components/ui/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
@@ -9,8 +9,8 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import {
   Card,
   CardContent,
@@ -18,41 +18,34 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import { HomePage } from './pages/Home/HomePage'
+} from "@/components/ui/card";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { HomePage } from "./pages/Home/HomePage";
 
 function App() {
-
   const urlServer = "http://127.0.0.1:8080/";
   const urlApi = urlServer + "auth/login";
   const [loginSuccessful, setLoginSuccessful] = useState(false);
-
 
   const formSchema = z.object({
     username: z.string().min(2, {
       message: "Username must be at least 2 characters.",
     }),
-    password: z.string()
-  })
+    password: z.string(),
+  });
 
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        username: "",
-        password: ""
-      },
-    })
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
-     // 2. Define a submit handler.
+  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-    
-
     const data = {
       username: values.username,
       password: values.password,
@@ -75,63 +68,124 @@ function App() {
       .catch((error) => {
         console.log(error);
       });
-    
   }
 
   return (
     <>
-    {loginSuccessful ? <HomePage/> : 
-    <div className="flex justify-center items-center h-screen">
-    <Card className='w-[350px] mx-auto my-auto rounded border-black border-2'>
-          <CardHeader>
-            <CardTitle>GymNotes</CardTitle>
-            <CardDescription>Login</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-                <FormField
-                  control={form.control}
-                  name="username"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Username</FormLabel>
-                      <FormControl>
-                        <Input placeholder="username" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input type='password' placeholder="password" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit">Sign in</Button>
-              </form>
-            </Form>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-          <Button asChild variant={'destructive'}>
-            <Link to="/register">Register</Link>
-          </Button>
-          </CardFooter>
-        </Card>
-    </div>}
-    
+      {loginSuccessful ? (
+        <HomePage />
+      ) : (
+        <>
+          <div className="md:hidden h-screen">
+            <img
+              src="/examples/authentication-light.png"
+              alt="Authentication"
+              className="block dark:hidden w-full h-full object-cover"
+            />
+            <img
+              src="/examples/authentication-dark.png"
+              alt="Authentication"
+              className="hidden dark:block w-full h-full object-cover"
+            />
+          </div>
+          <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+            <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+              <div className="absolute inset-0 bg-zinc-700" />
+              <div className="relative z-20 flex items-center text-lg font-medium">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2 h-6 w-6"
+                >
+                  <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+                </svg>
+                Gymnotes
+              </div>
+            </div>
+            <div className="lg:p-8">
+              <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <div className="flex justify-center items-center flex-col gap-4">
+                  <Card className="w-[350px] mx-auto my-auto rounded border-black border-2">
+                    <CardHeader>
+                      <CardTitle>GymNotes</CardTitle>
+                      <CardDescription>Login</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      <Form {...form}>
+                        <form
+                          onSubmit={form.handleSubmit(onSubmit)}
+                          className="space-y-2"
+                        >
+                          <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                  <Input placeholder="username" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                              </FormItem>
+                            )}
+                          />
+                          <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                  <Input
+                                    type="password"
+                                    placeholder="password"
+                                    {...field}
+                                  />
+                                </FormControl>
+                                <FormMessage />
+                              </FormItem>
+                            )}
+                          />
+                          <Button type="submit">Sign in</Button>
+                        </form>
+                      </Form>
+                    </CardContent>
+                    <CardFooter className="flex justify-between">
+                      <Button asChild variant={"destructive"}>
+                        <Link to="/register">Register</Link>
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                  <p className="px-8 text-center text-sm text-muted-foreground">
+                    By clicking continue, you agree to our{" "}
+                    <Link
+                      to="/terms"
+                      className="underline underline-offset-4 hover:text-primary"
+                    >
+                      Terms of Service
+                    </Link>{" "}
+                    and{" "}
+                    <Link
+                      to="/privacy"
+                      className="underline underline-offset-4 hover:text-primary"
+                    >
+                      Privacy Policy
+                    </Link>
+                    .
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
-    
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,3 @@
 export const HomePage = () => {
-    return(
-        <h1 className="text-green-600 m-7 text-3xl">Esta es la home page</h1>
-    )
-}
+  return <h1 className="text-green-600 m-7 text-3xl">Esta es la home page</h1>;
+};
